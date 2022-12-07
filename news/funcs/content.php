c7279d5834a3fca24c017f42473ae9a6f3481f2b
Ta Duc Hoan <taduchoan5503@gmail.com>
2022-12-07 21:46:27 +0700

    hoàn sửa license

--- news/funcs/content.php
+++ news/funcs/content.php
@@ -4,12 +4,14 @@ use NukeViet\Module\news\Shared\Logs;
 
 /**
  * NukeViet Content Management System
+ * DEARNASOUL Team
  * @version 4.x
  * @author VINADES.,JSC <contact@vinades.vn>
  * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
- * @license GNU/GPL version 2 or any later version
+ * @author DEARNASOUL
+ * @license GPL-3.0
  * @see https://github.com/nukeviet The NukeViet CMS GitHub project
- */
+*/ 
 
 if (!defined('NV_IS_MOD_NEWS')) {
     exit('Stop!!!');
