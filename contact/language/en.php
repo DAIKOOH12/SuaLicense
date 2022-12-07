abb8899c2fe1b809f7c70019327cd1c8e2d8aad4
Pham Duy Truong <daikooh1234@gmail.com>
2022-12-07 21:35:02 +0700

    Truong da sua lisence

--- contact/language/en.php
+++ contact/language/en.php
@@ -1,12 +1,11 @@
 <?php
 
 /**
- * NukeViet Content Management System
+ * DEARNASOUL Team
  * @version 4.x
- * @author VINADES.,JSC <contact@vinades.vn>
- * @copyright (C) 2009-2021 VINADES.,JSC. All rights reserved
- * @license GNU/GPL version 2 or any later version
- * @see https://github.com/nukeviet The NukeViet CMS GitHub project
+ * @author DEARNASOUL
+ * @license GPL-3.0
+ * @see https://github.com/DAIKOOH12/QLY_SODAUBAI The DEARNASOUL's project
  */
 
 if (!defined('NV_MAINFILE')) {
